267be4ae22428901d6993115631bccd25f8477c7
Victoria Fields <victoriaashleyfields@gmail.com>
2022-05-18 20:16:11 -0600

    Add annotations

--- variables.js
+++ variables.js
@@ -1,8 +1,10 @@
+// declare variable name and assign string value "String1"
 var name = "String1";
+// declare variable otherName and assign string value "String2"
 var otherName = "String2";
 
 var number = 1;
 var otherNumber = 2;
 
 var boolean = true;
-var otherBoolean = false; 
+var otherBoolean = false;
